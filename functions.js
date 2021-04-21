3f88b81c35b165d775b6baa9bbe7192232878563
Alina Bondarenko <alinabondarenko37@gmail.com>
2021-04-21 15:27:10 +0300

    Changes

---
+++ functions.js
@@ -0,0 +1,99 @@
+/*var bar = sum;
+
+function showMessage(string) {
+  console.log(string);
+}
+
+showMessage("Hellow");
+
+function sum(num1,num2) {
+
+  return num1 + num2;;
+}
+
+function () {
+  console.log("Good");
+}
+
+ function () {
+  console.log("Bed");
+}
+
+const getGood = (num) => {
+  console.log("Good",num);
+}
+
+
+function checkNum(num,succes,error) {
+  if(num === 5){
+    succes();
+  }
+  else{
+    error();
+  }
+}
+
+(function () {
+  console.log("Hi");
+})()
+
+function showMass(string) {
+  console.log(arguments);
+}
+
+function getCounter(){
+  var counter = 0;
+
+  return function(){
+    return ++counter;
+  }
+}
+
+function showContext(){
+  console.log(this);
+}
+
+function getCounter(){
+  var counter = 0;
+
+  return function(){
+    return ++counter;
+  }
+}
+////////////////////////////////////////////////////////////////////////
+var count = 0;
+
+function getCount()
+{
+    
+    count++;
+    console.log(count);
+}
+  
+
+var obj = {
+  str: 's',
+  b: false,
+};
+
+var arr = ['s', false, 1]
+
+function chengeVar(nObj,nArry) {
+  nObj.str += 'n';
+  nObj.newProp = 'aaaaa';
+  nArry[0] += 'q';
+  nArry[1] += true;
+  nArry[2] += 3;
+  nArry.push('newString');
+  console.log('chengeVar',nObj,nArry );
+}
+
+chengeVar(obj,arr);
+
+
+
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2, 4));
+*/
